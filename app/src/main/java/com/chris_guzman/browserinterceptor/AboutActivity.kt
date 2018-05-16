0a9978ed07ff1dc27c181b0b717594b0334bde78
Chris Guzman <guzmantchris@gmail.com>
2018-05-16 07:55:36 -0500

    Add Anko and better Kotlin

--- app/src/main/java/com/chris_guzman/browserinterceptor/AboutActivity.kt
+++ app/src/main/java/com/chris_guzman/browserinterceptor/AboutActivity.kt
@@ -5,6 +5,8 @@ import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_about.*
+import org.jetbrains.anko.browse
+import org.jetbrains.anko.share
 
 class AboutActivity : AppCompatActivity() {
 
@@ -14,30 +16,24 @@ class AboutActivity : AppCompatActivity() {
         title = "How to use Browser Interceptor"
 
         github.setOnClickListener{
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/ChrisGuzman/BrowserInterceptor"))
-            startActivity(intent)
+            browse("https://github.com/ChrisGuzman/BrowserInterceptor")
         }
 
         chris_site.setOnClickListener{
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://chris-guzman.com/"))
-            startActivity(intent)
+            browse("http://chris-guzman.com/")
         }
 
         about_pocket.setOnClickListener{
             val appPackageName = "com.ideashower.readitlater.pro"
             try {
-                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)))
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$appPackageName")))
             } catch (anfe: android.content.ActivityNotFoundException) {
-                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + appPackageName)))
+                browse("http://play.google.com/store/apps/details?id=$appPackageName")
             }
         }
 
         share_button.setOnClickListener{
-            val sendIntent = Intent()
-            sendIntent.action = Intent.ACTION_SEND
-            sendIntent.putExtra(Intent.EXTRA_TEXT, "Check out Browser Interceptor on the Play Store. chris-guzman.com/browser_intercept.html")
-            sendIntent.type = "text/plain"
-            startActivity(sendIntent)
+            share("Check out Browser Interceptor on the Play Store. chris-guzman.com/browser_intercept.html")
         }
     }
 }
--- app/src/main/java/com/chris_guzman/browserinterceptor/BrowserInterceptActivity.kt
+++ app/src/main/java/com/chris_guzman/browserinterceptor/BrowserInterceptActivity.kt
@@ -3,53 +3,38 @@ package com.chris_guzman.browserinterceptor
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.text.TextUtils
-import android.util.Log
-import android.widget.Toast
+import org.jetbrains.anko.longToast
 
 
 class BrowserInterceptActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val action = intent.action
-        var dataString: String
-        if (Intent.ACTION_VIEW.equals(action)) {
-            dataString = intent.dataString
 
-            if (TextUtils.isEmpty(dataString)) {
-                showError()
-                return
-            } else {
-                openInPocket(dataString)
-            }
+        if (Intent.ACTION_VIEW == intent.action && intent.dataString.isNotEmpty()) {
+            openInPocket(intent.dataString)
+        } else {
+            showError()
         }
 
         finishAffinity()
     }
 
     private fun openInPocket(dataString: String) {
-        val intent = Intent()
-        intent.setClassName("com.ideashower.readitlater.pro", "com.ideashower.readitlater.activity.AddActivity")
-        intent.putExtra(Intent.EXTRA_TEXT, dataString)
-
-        if (isPocketAvailable(intent)) {
-            startActivity(intent)
-        } else {
-            showError()
+        val pocketIntent = Intent().apply {
+            setClassName("com.ideashower.readitlater.pro", "com.ideashower.readitlater.activity.AddActivity")
+            putExtra(Intent.EXTRA_TEXT, dataString)
         }
+
+        if (pocketIsAvailable(pocketIntent)) startActivity(pocketIntent) else showError()
 
         finish()
     }
 
-    private fun isPocketAvailable(intent: Intent): Boolean {
-        val packageManager = packageManager ?: return false
-        val activities = packageManager.queryIntentActivities(intent, 0)
-
-        return activities.size > 0
-    }
+    private fun pocketIsAvailable(pocketIntent: Intent) =
+            packageManager?.queryIntentActivities(pocketIntent, 0)?.isNotEmpty() == true
 
     private fun showError() {
-        Toast.makeText(this@BrowserInterceptActivity, "Error opening in Pocket", Toast.LENGTH_LONG).show()
+        longToast("Error opening in Pocket")
     }
 }
